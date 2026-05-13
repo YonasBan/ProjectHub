fdc3968a8d48e6f1bfaf1e5f616df8b223a7e4b0
banyx <banyongxiang1@126.com>
2026-05-13 10:28:25 +0800

    添加appsettings

---
+++ ProjectHub.Application/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectHub.Application
+{
+    public class AppSettings
+    {
+
+    }
+}
--- ProjectHub.Presentation.Wpf/App.xaml.cs
+++ ProjectHub.Presentation.Wpf/App.xaml.cs
@@ -126,6 +126,7 @@ public partial class App : System.Windows.Application
 
         // ========== Register Infrastructure Services ==========
         services.AddInfrastructureServices(configuration);
+        services.Configure<AppSettings>(context.Configuration.GetSection("AppSettings"));
     }
 
     private void RegisterDialogs(IServiceCollection services)
